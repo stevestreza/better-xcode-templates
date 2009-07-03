ab8a2832abffc768d47d71bf1acd65537cc59fcf
Steve Streza <stevestreza@gmail.com>
2009-07-03 18:46:47 -0400

    Added Category template

---
+++ Templates/Foundation/Category.pbfiletemplate/category.h
@@ -0,0 +1,16 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface <#Class Name#> (<#Category Name#>)
+
+//STUB add your category method declarations here
+
+@end
---
+++ Templates/Foundation/Category.pbfiletemplate/category.m
@@ -0,0 +1,15 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+«OPTIONALHEADERIMPORTLINE»
+
+@implementation <#Class Name#> (<#Category Name#>)
+
+//STUB add your category method implementations here
+
+@end
