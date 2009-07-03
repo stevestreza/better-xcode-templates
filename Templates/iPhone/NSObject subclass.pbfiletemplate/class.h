5885cf7f3128b77da3b2993c340fd5e86d6ce85f
Steve Streza <stevestreza@gmail.com>
2009-07-03 17:07:40 -0400

    Added install directions to README

---
+++ Templates/iPhone/NSObject subclass.pbfiletemplate/class.h
@@ -0,0 +1,16 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface «FILEBASENAMEASIDENTIFIER» : NSObject {
+
+}
+
+@end
